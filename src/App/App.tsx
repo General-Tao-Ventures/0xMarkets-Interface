b523c45889e73f6f928c8c27ff92011357bda7eb
Raleigh, CA <kennethxashley@gmail.com>
2026-03-11 15:58:21 -0400

    Update testnet banner with discord link and bug reporting prompt

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -64,9 +64,13 @@ function App() {
   let app = (
     <>
       <div className="bg-yellow-500/20 text-center text-16 font-medium text-yellow-300 py-12 px-16">
-        You are using the testnet environment. Funds are not real.{" "}
-        <a href="https://faucet.0xMarkets.io" target="_blank" rel="noopener noreferrer" className="underline hover:text-white">
-          Get testnet tokens
+        You are using the testnet environment. Funds are not real. Please report any issues/bugs in{" "}
+        <a href="https://discord.gg/d87vC7uaNx" target="_blank" rel="noopener noreferrer" className="underline hover:text-white">
+          discord
+        </a>
+        .{" "}
+        <a href="https://faucet.0xMarkets.io" target="_blank" rel="noopener noreferrer" className="uppercase font-bold underline hover:text-white">
+          Get Testnet Tokens
         </a>
       </div>
       <KeeperStatusBanner />
