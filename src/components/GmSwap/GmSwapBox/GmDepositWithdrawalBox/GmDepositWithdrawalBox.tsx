0303c9cb4508b3b8d1ebca3d3921b1dcd73d1858
Tony <tony.leviethung@gmail.com>
2026-03-28 00:04:18 -0700

    Refactor GmDepositWithdrawalBox component imports for improved organization and clarity

--- src/components/GmSwap/GmSwapBox/GmDepositWithdrawalBox/GmDepositWithdrawalBox.tsx
+++ src/components/GmSwap/GmSwapBox/GmDepositWithdrawalBox/GmDepositWithdrawalBox.tsx
@@ -39,11 +39,15 @@ import TokenWithIcon from "components/TokenIcon/TokenWithIcon";
 import TokenSelector from "components/TokenSelector/TokenSelector";
 import TooltipWithPortal from "components/Tooltip/TooltipWithPortal";
 
-import type { GmSwapBoxProps } from "../GmSwapBox";
-import { GmSwapBoxPoolRow } from "../GmSwapBoxPoolRow";
 import { useDepositAmountOut } from "domain/synthetics/markets/useDepositAmountOut";
 
+import type { GmSwapBoxProps } from "../GmSwapBox";
+import { GmSwapBoxPoolRow } from "../GmSwapBoxPoolRow";
 import { GmSwapWarningsRow } from "../GmSwapWarningsRow";
+import { SelectedPool } from "../SelectedPool";
+import { Mode, Operation } from "../types";
+import { useGmWarningState } from "../useGmWarningState";
+import { InfoRows } from "./InfoRows";
 
 import { useDepositWithdrawalAmounts } from "./useDepositWithdrawalAmounts";
 import { useDepositWithdrawalFees } from "./useDepositWithdrawalFees";
