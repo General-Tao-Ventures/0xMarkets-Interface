519137e8ae93a3c7f671275e8f01839a5e49fe55
goz1e <phenominal55@gmail.com>
2026-08-06 14:00:04 +0100

    banner fix

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -63,6 +63,27 @@ function App() {
 
   let app = (
     <>
+      <div className="bg-yellow-500/20 px-16 py-12 text-center text-16 font-normal text-yellow-300">
+        <span className="font-medium">Beta Mainnet Live:</span> You're early enough to find a bug. Spot something broken
+        or unexpectedly spicy? Let us know on{" "}
+        <a
+          href="https://discord.gg/d87vC7uaNx"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline hover:text-white"
+        >
+          Discord
+        </a>
+        .{" "}
+        <a
+          href="https://faucet.0xMarkets.io"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline hover:text-white"
+        >
+          Get testnet tokens
+        </a>
+      </div>
       <KeeperStatusBanner />
       <AppRoutes />
     </>
